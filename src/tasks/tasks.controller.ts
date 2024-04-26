be75a3eae053e205b1eb981e4a4a61cf916b48c1
Tecnico <Tecnico@NB-INFOR-001006.FUTURO.LOCAL>
2024-04-26 16:23:42 -0400

    add Request n Response

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -1,18 +1,28 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, Post, Req, Res } from '@nestjs/common';
 import { TasksService } from './tasks.service';
+import { Request, Response } from 'express';
 
-@Controller({})
+@Controller()
 export class TasksController {
 
-    constructor(private tasksService: TasksService) {}
+    constructor(private tasksService: TasksService) { }
 
     @Get('/')
-    index() {
-        return { msg: 'Nestjs API...'}
+    index(@Req() request: Request, @Res() response: Response) {
+        response.status(200).json({
+            msg: 'Nestjs API...'
+        })
+        //return { msg: 'Nestjs API...'}
     }
 
     @Get('/tasks')
     getAll() {
         return this.tasksService.obtenerTareas(); //{ msg: 'Obteniendo todas las tareas...'}
+    }
+
+    @Post('/tasks')
+    post(@Body() task: any) {
+        this.tasksService.crearTarea(task);
+        return task;
     }
 }
--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -2,7 +2,14 @@ import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class TasksService {
+    private tasks = [];
+
     obtenerTareas() {
-        return ['Task 1', 'Task 2', 'Task 3'];
+        return this.tasks;
+    }
+
+    crearTarea(task: any) {
+        this.tasks.push(task);
+        console.log('TaskService: ', task);
     }
 }
