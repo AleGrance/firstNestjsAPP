419b049686078fbccb3e0575e2542f2bd426216d
Tecnico <Tecnico@NB-INFOR-001006.FUTURO.LOCAL>
2024-04-29 23:25:17 -0400

    nest g res products

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,10 @@ import { UsersModule } from './users/users.module';
 import { AuthModule } from './auth/auth.module';
 import { ProjectsModule } from './projects/projects.module';
 import { AppController } from './app.controller';
+import { ProductsModule } from './products/products.module';
 
 @Module({
-  imports: [TasksModule, UsersModule, AuthModule, ProjectsModule],
+  imports: [TasksModule, UsersModule, AuthModule, ProjectsModule, ProductsModule],
   controllers: [AppController]
 })
 export class AppModule {}
